e34106a80d917d15250a5cdc0c868b04f0159fbc
Yongran <maiyongran@gmail.com>
2022-09-05 14:37:44 +0100

    add Get request

--- src/main/java/com/example/task_names/controller/GreetingController.java
+++ src/main/java/com/example/task_names/controller/GreetingController.java
@@ -1,12 +1,15 @@
-package controller;
+package com.example.task_names.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/greeting")
+//@RequestMapping(value = "/greeting")
 public class GreetingController {
 
-   
+    @GetMapping("/greeting")
+    public String greeting(){
+        return "Good afternoon!";
+    }
 }
