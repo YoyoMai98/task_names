1eef4a9d97cef8ee8a0b698275ecb01f99d5b5e6
Yongran <maiyongran@gmail.com>
2022-09-05 14:46:40 +0100

    use RequestParam

--- src/main/java/com/example/task_names/controller/GreetingController.java
+++ src/main/java/com/example/task_names/controller/GreetingController.java
@@ -3,6 +3,7 @@ package com.example.task_names.controller;
 import com.example.task_names.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     @GetMapping
-    public Greeting greeting(){
-        return new Greeting("Yongran", "14:41");
+    public Greeting greeting(@RequestParam(value = "timeOfDay") String timeOfDay){
+        return new Greeting("Yongran", timeOfDay);
     }
 }
