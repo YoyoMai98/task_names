a40a8571046b766e3072370a275659ca333daadf
Yongran <maiyongran@gmail.com>
2022-09-05 14:47:46 +0100

    add a Celebration DTO

---
+++ src/main/java/com/example/task_names/models/Celebation.java
@@ -0,0 +1,20 @@
+package com.example.task_names.models;
+
+public class Celebation {
+
+    private String message;
+
+    public Celebation (String message){
+        this.message = message;
+    }
+
+    public Celebation (){}
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
