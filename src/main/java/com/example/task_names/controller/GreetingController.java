c152d796bebe85eaa99f682efcc0fb0a824987ca
Yongran <maiyongran@gmail.com>
2022-09-05 14:42:23 +0100

    refactor controller

--- src/main/java/com/example/task_names/controller/GreetingController.java
+++ src/main/java/com/example/task_names/controller/GreetingController.java
@@ -1,15 +1,16 @@
 package com.example.task_names.controller;
 
+import com.example.task_names.models.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping(value = "/greeting")
+@RequestMapping(value = "/greeting")
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(){
-        return "Good afternoon!";
+    @GetMapping
+    public Greeting greeting(){
+        return new Greeting("Yongran", "14:41");
     }
 }
