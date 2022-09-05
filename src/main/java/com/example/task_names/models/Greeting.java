91d6ed43930f50c1fa2015c4ca6646d23d0a4823
Yongran <maiyongran@gmail.com>
2022-09-05 14:40:13 +0100

    create a POJO

---
+++ src/main/java/com/example/task_names/models/Greeting.java
@@ -0,0 +1,30 @@
+package com.example.task_names.models;
+
+public class Greeting {
+
+    private String name;
+    private String timeOfDay;
+
+    public Greeting(String name, String timeOfDay) {
+        this.name = name;
+        this.timeOfDay = timeOfDay;
+    }
+
+    public Greeting (){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    public void setTimeOfDay(String timeOfDay) {
+        this.timeOfDay = timeOfDay;
+    }
+}
